94ef65a3d82427c6548f36f4a0ef3941ecc34379
Mattt Switlik <switlik@oakland.edu>
2012-04-18 16:26:47 -0400

    OUMoodle internal_email migration script lists files with paths with a preceding "*" if not yet  migrated.

--- mod/email/email_migrate_19_files.php
+++ mod/email/email_migrate_19_files.php
@@ -6,63 +6,70 @@ require_once('../../config.php');
 
 require_login();
 
+$fs = get_file_storage();
+
+
 //Get all courses with Internal Email
-$sql = "SELECT course";
+$sql = "SELECT";
+    $sql.= " id,";
+    $sql.= " course";
 $sql.= " FROM mdl_email";
 $sql.= " WHERE course=72278";
-$sql.= " GROUP BY course";
+//$sql.= " GROUP BY course";
 $sql.= ";";
-$courses = $DB->get_records_sql($sql);
-foreach($courses as $course){
-    $dir_email = $CFG->dataroot."/".$course->course."/moddata/email";
-    if(file_exists($dir_email)){
-        echo $dir_email."<br/>\n";
-        echo "<hr/>\n";
-        if(is_dir($dir_email))
+$emails = $DB->get_records_sql($sql);
+foreach($emails as $email){
+    echo "<hr/>\n";
+    
+    if (! $cm = get_coursemodule_from_instance("email", $email->id, $email->course)) {
+        exit;
+    }
+    if (! $context = context_module::instance($cm->id)){
+        exit;
+    }
+                                            
+    
+    $dir_email = $CFG->dataroot."/".$email->course."/moddata/email";
+    if(file_exists($dir_email) && is_dir($dir_email))
+    {
+        //Accounts
+        if($h_account = opendir($dir_email))
         {
-            //Accounts
-            if($h_account = opendir($dir_email))
+            while(($dir_account = readdir($h_account)) !== false)
             {
-                while(($dir_account = readdir($h_account)) !== false)
+                if($dir_account != "." && $dir_account != ".." && is_dir($dir_email."/".$dir_account))
                 {
-                    if($dir_account != "." && $dir_account != ".." && is_dir($dir_email."/".$dir_account))
+                    //Mails
+                    if($h_mail = opendir($dir_email."/".$dir_account))
                     {
-                        echo "_ ".$dir_account."<br>\n";
-                        
-                        //Mails
-                        if($h_mail = opendir($dir_email."/".$dir_account))
+                        while(($dir_mail = readdir($h_mail)) !== false)
                         {
-                            while(($dir_mail = readdir($h_mail)) !== false)
+                            if($dir_mail != "." && $dir_mail != ".." && is_dir($dir_email."/".$dir_account."/".$dir_mail))
                             {
-                                if($dir_mail != "." && $dir_mail != ".." && is_dir($dir_email."/".$dir_account."/".$dir_mail))
+                                //File Attachments
+                                if($h_file = opendir($dir_email."/".$dir_account."/".$dir_mail))
                                 {
-                                    echo "_ _ ".$dir_mail."<br>\n";
-                                    
-                                    //File Attachments
-                                    if($h_file = opendir($dir_email."/".$dir_account."/".$dir_mail))
-                                    {
-                                        while(($filename = readdir($h_file)) !== false){
-                                            if($filename != "." && $filename != ".." && !is_dir($dir_email."/".$dir_account."/".$dir_mail."/".$dir_mail."/".$filename))
-                                            {
-                                                echo "_ _ _ ".$filename."<br/>\n";
-                                            }      
-                                        }
-                                        closedir($h_file);
-                                     
+                                    while(($filename = readdir($h_file)) !== false){
+                                        if($filename != "." && $filename != ".." && !is_dir($dir_email."/".$dir_account."/".$dir_mail."/".$dir_mail."/".$filename))
+                                        {
+                                            $file = $fs->get_file($context->id, 'mod_email', 'attachments', $dir_mail, "/", $filename);
+                                            if($file===false){
+                                                echo "*";
+                                                //Copy the file to the repo.
+                                            }
+                                            echo $dir_email."/".$dir_account."/".$dir_mail."/".$dir_mail."/".$filename."<br/>\n";
+                                            
+                                        }      
                                     }
-                                    //
-                                    
+                                    closedir($h_file);
                                 }
                             }
-                            closedir($h_mail);
                         }
-                        //
-                        
+                        closedir($h_mail);
                     }
                 }
-                closedir($h_account);
             }
-            //
+            closedir($h_account);
         }
     }
 }
