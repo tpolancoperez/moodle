03661a1045630888852bda2e4795d255f64e22fc
Eric Merrill <merrill@oakland.edu>
2012-06-27 10:45:51 -0400

    OUMoodle attendance_module Fix for capabilities

--- mod/attforblock/version.php
+++ mod/attforblock/version.php
@@ -5,7 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2011111700;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2011111701;  // The current module version (Date: YYYYMMDDXX)
 $module->release = '2.1.1.1';
 $module->cron     = 0;           // Period for cron to check this module (secs)
 ?>
