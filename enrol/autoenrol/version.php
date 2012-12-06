170f6af2bb16de04978abd2d845157c7a8ba8d7c
tpolancoperez <tpolancoperez@gmail.com>
2012-12-06 10:55:27 -0800

    Plugin: Autoenrol - automatically enrols users onto a course

---
+++ enrol/autoenrol/version.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * autoenrol enrolment plugin.
+ *
+ * This plugin automatically enrols a user onto a course the first time they try to access it.
+ *
+ * @package    enrol
+ * @subpackage autoenrol
+ * @author     Mark Ward & Matthew Cannings - based on code by Martin Dougiamas, Petr Skoda, Eugene Venter and others
+ * @date       September 2012
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2012092700;
+$plugin->requires = 2010112400;   // Requires this Moodle version
+$plugin->release = '0.91'; //plugin release
+$plugin->maturity   = MATURITY_BETA;	
