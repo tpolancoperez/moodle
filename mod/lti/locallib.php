88d8cdd14b7a1a04e158c2d4734a9466fe64b9a9
mike <michaelseiler@fuller.edu>
2013-02-12 15:53:36 -0800

    Changed LTI for Ares eReserves Integration
    Changes to $requestparams; added $USER->username, and modified $USER->id
    to $USER->user_id, which in our instance is the "idnumber" or the
    immutable Banner LMB number from the LMB Enroll XML

--- mod/lti/locallib.php
+++ mod/lti/locallib.php
@@ -221,7 +221,7 @@ function lti_build_sourcedid($instanceid, $userid, $launchid = null, $servicesal
  */
 function lti_build_request($instance, $typeconfig, $course) {
     global $USER, $CFG;
-
+	
     if (empty($instance->cmid)) {
         $instance->cmid = 0;
     }
@@ -232,12 +232,20 @@ function lti_build_request($instance, $typeconfig, $course) {
     if ( strlen($locale) < 1 ) {
          $locale = $CFG->lang;
     }
-
+	////////////////////////////////////////////
+	// 2/12/13 - MIKE SEILER x5237
+	// Modified to use the "sourced_id" from the
+	// Banner LMB enrollment, which in our
+	// instance gets put into 'idnumber' in the
+	// database; use this instead of $USER->id.  
+	// Also added 'username'
+	////////////////////////////////////////////
     $requestparams = array(
         'resource_link_id' => $instance->id,
         'resource_link_title' => $instance->name,
         'resource_link_description' => $instance->intro,
-        'user_id' => $USER->id,
+        'user_id' => $USER->idnumber,
+		'username' => $USER->username,
         'roles' => $role,
         'context_id' => $course->id,
         'context_label' => $course->shortname,
