7684d34819a979b664b2cc2ccb6ae93cb6ef8b7f
tpolancoperez <tpolancoperez@gmail.com>
2013-06-05 10:22:38 -0700

    Mod: recordingsbn

--- mod/recordingsbn/deploy.sh
+++
@@ -1,4 +0,0 @@
-sudo rm -rfv /var/www/default/htdocs/moodle22/mod/recordingsbn/*
-sudo cp -rv . /var/www/default/htdocs/moodle22/mod/recordingsbn/
-sudo rm -rfv /var/www/default/htdocs/moodle23/mod/recordingsbn/*
-sudo cp -rv . /var/www/default/htdocs/moodle23/mod/recordingsbn/
